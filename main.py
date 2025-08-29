97ebac180cdbee93bd96536bb6839587f58cbced
tanny <tanadonthara@gmail.com>
2025-08-29 19:19:12 +0700

    Print final report with END footer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, get_char_counts, get_sorted_char_counts
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,6 +8,19 @@ def main():
     text = get_book_text("books/frankenstein.txt")
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
+
+    char_counts = get_char_counts(text)
+    print(char_counts)
+    sorted_chars = get_sorted_char_counts(char_counts)
+
+    print("=========== BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("---------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("------- Character Count -------")
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+    print("============ END =============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -1,2 +1,23 @@
 def count_words(text):
     return len(text.split())
+
+def get_char_counts(text):
+    counts = {}
+    for ch in text.lower():
+        if ch in counts:
+    	    counts[ch] += 1
+        else:
+            counts[ch] = 1
+    return counts
+
+def get_sorted_char_counts(counts):
+    items = []
+    for ch in counts:
+        if ch.isalpha():              # skip non-letters
+            items.append({"char": ch, "num": counts[ch]})
+
+    def sort_on(d):
+        return d["num"]
+
+    items.sort(key=sort_on, reverse=True)  # greatest to least
+    return items
